79b545c69c0cf01f329bb4b7a7adfbf711376d09
Gevilee <157779885+gverango@users.noreply.github.com>
2025-10-11 00:38:26 -0700

    Update full_screen_control.gd
    dunno if this actually works on mac

--- cali-clash/scenes/full_screen_control.gd
+++ cali-clash/scenes/full_screen_control.gd
@@ -4,14 +4,7 @@ var prev_mode := Window.MODE_WINDOWED
 
 func _on_toggled(toggled_on: bool) -> void:
 	print("toggled")
-
-	var win := get_window()
-	if win == null:
-		print("No window found!")
-		return
-
-	if toggled_on:
-		prev_mode = win.mode
-		win.mode = Window.MODE_FULLSCREEN
-	else:
-		win.mode = Window.MODE_WINDOWED
+	if (toggled_on == true):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else: 
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
