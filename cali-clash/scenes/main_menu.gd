e4b74ec7db0ddc65d22e46f5622fe8478be70946
Gevilee <157779885+gverango@users.noreply.github.com>
2025-10-12 01:06:21 -0700

    imported lots UI and assets

--- cali-clash/scenes/main_menu.gd
+++ cali-clash/scenes/main_menu.gd
@@ -78,7 +78,7 @@ func _on_final_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/battle.tscn")
 	
 func _on_start_button_pressed() -> void:
-	print("start pressed")
+	Dialogic.start("introduction")
 func _on_options_button_pressed() -> void:
 	main_buttons.visible = false
 	options.visible = true
